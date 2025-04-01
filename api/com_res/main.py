8c840113ed9b8243d392770b643b6f457b089f4d
Anthony Castronova <castronova.anthony@gmail.com>
2025-04-01 09:46:42 -0400

    linting/formatting

--- api/com_res/main.py
+++ api/com_res/main.py
@@ -2,7 +2,6 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
 from app.routers.timeseries import router as timeseries_router
-
 from app.users import cuahsi_oauth_client
 from config import get_settings
 
