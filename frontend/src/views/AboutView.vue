84c9978ed7c9d7cbeb921512952418d656b45c7a
Devin Cowan <dcowan@cuahsi.org>
2025-04-16 19:32:19 -0400

    refactor: standardize string quotes in AboutView and HelpView

--- frontend/src/views/AboutView.vue
+++ frontend/src/views/AboutView.vue
@@ -3,7 +3,7 @@
   <v-container class="about">
     <v-sheet class="pa-6 mx-auto ma-4" max-width="1200" rounded>
       <p>
-        {{ loremIpsum({ count: 3, units: "paragraphs" }) }}
+        {{ loremIpsum({ count: 3, units: 'paragraphs' }) }}
       </p>
     </v-sheet>
   </v-container>
@@ -49,7 +49,7 @@
 </style>
 
 <script setup>
-import { loremIpsum } from "lorem-ipsum";
+import { loremIpsum } from 'lorem-ipsum'
 const members = [
   {
     name: 'Tony Castronova',
--- frontend/src/views/HelpView.vue
+++ frontend/src/views/HelpView.vue
@@ -1,11 +1,11 @@
 <template>
   <h2 class="ma-2 text-center">Help</h2>
   <v-container>
-    {{ loremIpsum({ count: 2, units: "paragraphs" , paragraphLowerBound: 7}) }}
+    {{ loremIpsum({ count: 2, units: 'paragraphs', paragraphLowerBound: 7 }) }}
     <br /><br />
-    {{ loremIpsum({ count: 2, units: "paragraphs" , paragraphLowerBound: 7}) }}
+    {{ loremIpsum({ count: 2, units: 'paragraphs', paragraphLowerBound: 7 }) }}
   </v-container>
 </template>
 <script setup>
-import { loremIpsum } from "lorem-ipsum";
+import { loremIpsum } from 'lorem-ipsum'
 </script>
