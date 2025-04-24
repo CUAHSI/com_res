c3f272e29495cb713d24360e929a89ec99be5033
Devin Cowan <dcowan@cuahsi.org>
2025-04-24 13:06:15 -0400

    refactor: remove unused plot button from DataViewDrawer component

--- frontend/src/components/DataViewDrawer.vue
+++ frontend/src/components/DataViewDrawer.vue
@@ -18,15 +18,6 @@
       >
       </v-btn>
       <StaticMetadata />
-      <v-btn
-        v-if="!hasResults()"
-        @click="router.push(`/plots/${featureStore.activeFeature.properties.reach_id}`)"
-        color="primary"
-        class="ma-2"
-        :loading="featureStore.querying.hydrocron"
-      >
-        <v-icon :icon="mdiChartScatterPlot"></v-icon>Plot
-      </v-btn>
     </v-container>
   </v-navigation-drawer>
 </template>
@@ -34,9 +25,8 @@
 <script setup>
 import { ref } from 'vue'
 import { useFeaturesStore } from '@/stores/features'
-import { mdiChevronRight, mdiChevronLeft, mdiChartScatterPlot } from '@mdi/js'
+import { mdiChevronRight, mdiChevronLeft } from '@mdi/js'
 import StaticMetadata from './StaticMetadata.vue'
-import { useRouter } from 'vue-router'
 
 const featureStore = useFeaturesStore()
 
@@ -48,12 +38,6 @@ const translate = () => {
   } else {
     return 'translate(-180%, 0)'
   }
-}
-
-const router = useRouter()
-
-const hasResults = () => {
-  return featureStore?.activeFeature?.results !== undefined
 }
 
 featureStore.$subscribe((mutation, state) => {
