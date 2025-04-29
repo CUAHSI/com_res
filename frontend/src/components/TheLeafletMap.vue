be4053f8d8e5b1399073dffa4f94a08fa6bcac7f
Devin Cowan <dcowan@cuahsi.org>
2025-04-29 10:20:28 -0400

    feat: update Esri Hydro Reference Overlay and add CartoDB Positron No Labels layer

--- frontend/src/components/TheLeafletMap.vue
+++ frontend/src/components/TheLeafletMap.vue
@@ -37,28 +37,37 @@ onMounted(() => {
 
   mapObject.value.bbox = [99999999, 99999999, -99999999, -99999999]
 
-  let url =
-    'https://tiles.arcgis.com/tiles/P3ePLMYs2RVChkJx/arcgis/rest/services/Esri_Hydro_Reference_Overlay/MapServer'
-  // url = 'https://tiles.arcgis.com/tiles/P3ePLMYs2RVChkJx/arcgis/rest/services/Esri_Hydro_Reference_Labels/MapServer'
-
   let Esri_Hydro_Reference_Overlay = esriLeaflet.tiledMapLayer({
-    url: url,
+    url: 'https://tiles.arcgis.com/tiles/P3ePLMYs2RVChkJx/arcgis/rest/services/Esri_Hydro_Reference_Overlay/MapServer',
     layers: 0,
     transparent: 'true',
-    format: 'image/png'
-  })
-
-  url =
-    'https://server.arcgisonline.com/ArcGIS/rest/services/World_Topo_Map/MapServer/tile/{z}/{y}/{x}'
-  let Esri_WorldImagery = L.tileLayer(url, {
-    variant: 'World_Imagery',
-    attribution: 'Esri',
+    format: 'image/png',
     maxZoom: minReachSelectionZoom,
     minZoom: 0
   })
 
+  let CartoDB_PositronNoLabels = L.tileLayer(
+    'https://{s}.basemaps.cartocdn.com/light_nolabels/{z}/{x}/{y}{r}.png',
+    {
+      attribution:
+        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
+      subdomains: 'abcd',
+      maxZoom: 20
+    }
+  )
+
+  let url =
+    'https://server.arcgisonline.com/ArcGIS/rest/services/World_Topo_Map/MapServer/tile/{z}/{y}/{x}'
+  let Esri_WorldImagery = L.tileLayer(url, {
+    variant: 'World_Imagery',
+    attribution: 'Esri',
+    maxZoom: 18,
+    minZoom: 0
+  })
+
   const baselayers = {
-    Esri_WorldImagery
+    'ESRI World Imagery': Esri_WorldImagery,
+    'CartoDB Positron No Labels': CartoDB_PositronNoLabels
   }
 
   Esri_WorldImagery.addTo(leaflet)
