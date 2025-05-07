86c5bf28ac15ef176d9f1e54669bcf0b7c48b832
Anthony Castronova <castronova.anthony@gmail.com>
2025-05-07 11:33:56 -0400

    Adding project description to the home page

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -4,8 +4,16 @@
       <v-col>
         <div>
           <div>
-            <p class="font-weight-light text-subtitle">
-              {{ loremIpsum({ count: 2, units: 'paragraph' }) }}
+            <p class="font-weight-light text-subtitle" style="padding: 10px 20px">
+              This application was established to provide a user-centered perspective on integrating
+              the National Water Model (NWM) into community resilience planning and water
+              decision-making processes across selected regions in Vermont and Missouri. In Vermont,
+              the research focused on three regions: Two Rivers-Ottauquechee, Mt. Ascutney, and
+              Windham. In Missouri, the study engaged with three distinct communities: Springfield,
+              De Soto, and Roaring River State Park. The methods were designed to understand each
+              community's unique context, including their water-related vulnerabilities, resilience
+              planning constraints, and information and data gaps, to develop a series of resources
+              of the NWM that could best help aid their particular needs.
             </p>
           </div>
         </div>
@@ -34,7 +42,6 @@
 </template>
 
 <script setup>
-import { loremIpsum } from 'lorem-ipsum'
 import { useRouter } from 'vue-router'
 import { useRegionsStore } from '../stores/regions'
 
