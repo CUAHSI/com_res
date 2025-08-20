f5f774500ddcb722c886284cb27af25db1450fd4
igarousi <irene.garousi.nejad@gmail.com>
2025-08-19 22:43:17 -0400

    updated this file to improve text styling and make sure popup is not hidden by plots

--- frontend/src/components/InfoTooltip.vue
+++ frontend/src/components/InfoTooltip.vue
@@ -1,21 +1,37 @@
 <template>
-  <v-tooltip location="bottom" max-width="200px" style="white-space: normal">
-    <template #activator="{ props }">
+  <v-tooltip
+    :location="location"
+    :max-width="maxWidth"
+    :content-class="contentClass"
+    :z-index="zIndex"
+  >
+    <template #activator="{ props: act }">
       <v-icon
-        v-bind="props"
+        v-bind="act"
         :icon="mdiInformationOutline"
         color="info"
-        style="cursor: pointer; margin-left: 10px"
-      >
-      </v-icon>
+        class="cursor-pointer ml-2"
+      />
     </template>
-    <div>{{ props.text }}</div>
+
+    <div class="whitespace-normal">{{ text }}</div>
   </v-tooltip>
 </template>
 
 <script setup>
 import { mdiInformationOutline } from '@mdi/js'
-const props = defineProps({
-  text: { type: String, default: '' }
+
+const { text, contentClass, zIndex, location, maxWidth } = defineProps({
+  text: { type: String, default: '' },
+  contentClass: { type: String, default: '' }, // lets us target the teleported overlay
+  zIndex: { type: [Number, String], default: 200000 }, // render above plot container (99999)
+  location: { type: String, default: 'bottom' },
+  maxWidth: { type: [Number, String], default: 200 }
 })
 </script>
+
+<style scoped>
+.whitespace-normal { white-space: normal; }
+.ml-2 { margin-left: 10px; }
+.cursor-pointer { cursor: pointer; }
+</style>
