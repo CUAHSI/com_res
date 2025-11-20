67b1a1d13ee9be930721df2eae0e839064a7815c
Devin Cowan <dcowan@cuahsi.org>
2025-11-20 09:35:38 -0500

    remove cypress

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -1,15 +1,12 @@
-import js from "@eslint/js";
+import js from '@eslint/js'
 import pluginVue from 'eslint-plugin-vue'
 import globals from 'globals'
-// import cypress from "eslint-plugin-cypress";
 
 export default [
   js.configs.recommended,
   ...pluginVue.configs['flat/recommended'],
   {
-    rules: {
-      // ...cypress.configs.recommended.rules,
-    },
+    rules: {},
     languageOptions: {
       sourceType: 'module',
       globals: {
