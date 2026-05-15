29379385c0c21ced1d6590a729162c626698d4dc
Anthony Castronova <castronova.anthony@gmail.com>
2026-05-15 18:04:55 -0400

    added time picker to forecast plot. modified lineplot so that timepicker appears even if data is not found for the current date

--- api/com_res/app/routers/timeseries/historical.py
+++ api/com_res/app/routers/timeseries/historical.py
@@ -181,8 +181,10 @@ class AnalysisAssim:
 
         if len(dfs) > 1:
             df = pandas.concat(dfs, ignore_index=True)
-        else:
+        elif len(dfs) == 1:
             df = dfs[0]
+        else:
+            df = pandas.DataFrame(columns=["feature_id", "time", "streamflow", "velocity"])
 
         # clean datetime columns and return
         df.time = pandas.to_datetime(df.time)
